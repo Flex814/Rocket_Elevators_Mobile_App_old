10fb7b4b7d5852234f56fafc17002d7665b6f84f
Jacob Gomez <jakegomez814@hotmail.com>
2022-01-11 20:51:54 -0500

    init

---
+++ app/src/main/java/com/example/android/firebaseui_login_sample/elevatorData.kt
@@ -0,0 +1,3 @@
+package com.example.android.firebaseui_login_sample
+
+class elevatorData : ArrayList<elevatorDataItem>()
---
+++ app/src/main/java/com/example/android/firebaseui_login_sample/elevatorDataItem.kt
@@ -0,0 +1,18 @@
+package com.example.android.firebaseui_login_sample
+
+data class elevatorDataItem(
+    val certificateOfInspection: String,
+    val column: Any,
+    val columnId: Int,
+    val createdAt: String,
+    val dateOfCommissioning: String,
+    val dateOfLastInspection: String,
+    val elevatorType: String,
+    val id: Int,
+    val interventions: List<Any>,
+    val model: String,
+    val notes: String,
+    val serialNumber: String,
+    val status: String,
+    val updatedAt: String
+)
